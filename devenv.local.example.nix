f9279f356c467d212cd337b9afeed5bdd4be6a71
Asko Soukka <asko.soukka@iki.fi>
2025-05-14 21:32:45 +0300

    First commit

---
+++ devenv.local.example.nix
@@ -0,0 +1,9 @@
+{
+  package.vscodium.enable = true;
+  package.vscodium.python = true;
+  package.vscodium.robot = true;
+  package.vscodium.bpmn = true;
+  package.vscodium.unfree = true;
+  package.vscodium.vim = true;
+  package.vscodium.copilot= true;
+}
---
+++ devenv.nix
@@ -0,0 +1,108 @@
+{
+  pkgs,
+  config,
+  lib,
+  inputs,
+  ...
+}:
+let
+  cfg = config.package.vscodium;
+  unstable = import inputs.unstable {
+    system = pkgs.system;
+    config = {
+      allowUnfree = true;
+    };
+  };
+  vscode-marketplace =
+    (inputs.nix-vscode-extensions.extensions.${pkgs.system}.forVSCodeVersion unstable.vscodium.version)
+    .vscode-marketplace;
+  vscode-marketplace-release =
+    (inputs.nix-vscode-extensions.extensions.${pkgs.system}.forVSCodeVersion unstable.vscodium.version)
+    .vscode-marketplace-release;
+  inherit (lib)
+    types
+    mkOption
+    mkIf
+    optionals
+    ;
+in
+{
+  options.package.vscodium = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    bpmn = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    python = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    robot = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    unfree = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    vim = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    copilot = mkOption {
+      type = types.bool;
+      default = false;
+    };
+    continue = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+  config.packages = mkIf cfg.enable [
+    (unstable.vscode-with-extensions.override {
+      vscode = if cfg.unfree then unstable.vscode else unstable.vscodium;
+      vscodeExtensions =
+        [
+          unstable.vscode-extensions.ms-vscode.makefile-tools
+          vscode-marketplace.bbenoist.nix
+          vscode-marketplace.tamasfe.even-better-toml
+        ]
+        ++ optionals cfg.unfree [
+          (vscode-marketplace.ms-vscode-remote.remote-ssh.override { meta.licenses = [ ]; })
+          (unstable.vscode-extensions.ms-python.vscode-pylance) # .override { meta.licenses = [ ]; })
+        ]
+        ++ optionals (!cfg.unfree) [
+          vscode-marketplace.ms-pyright.pyright # cannot be used with pylance
+        ]
+        ++ optionals cfg.python or cfg.robot [
+          pkgs.vscode-extensions.ms-python.python
+          pkgs.vscode-extensions.ms-python.debugpy
+          (vscode-marketplace.charliermarsh.ruff.overrideAttrs (old: {
+            postInstall = ''
+              rm -f $out/share/vscode/extensions/charliermarsh.ruff/bundled/libs/bin/ruff
+              ln -s ${pkgs.ruff}/bin/ruff $out/share/vscode/extensions/charliermarsh.ruff/bundled/libs/bin/ruff
+            '';
+          }))
+        ]
+        ++ optionals cfg.robot [
+          vscode-marketplace.d-biehl.robotcode
+        ]
+        ++ optionals cfg.bpmn [
+          vscode-marketplace.miragon-gmbh.vs-code-bpmn-modeler
+        ]
+        ++ optionals cfg.vim [
+          vscode-marketplace.vscodevim.vim
+        ]
+        ++ optionals cfg.copilot [
+          (vscode-marketplace-release.github.copilot.override { meta.licenses = [ ]; })
+          (vscode-marketplace-release.github.copilot-chat.override { meta.licenses = [ ]; })
+        ]
+        ++ optionals cfg.continue [
+          vscode-marketplace.continue.continue
+        ];
+    })
+  ];
+}
